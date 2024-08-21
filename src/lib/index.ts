26259e8f6943376e3325731e8476e5589f0f5147
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2024-08-21 12:08:33 +0900

    feat!: disable submitter during submission

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -21,32 +21,38 @@ type ChangeReturnType<Function, Returns> = Function extends (...params: infer Pa
 type Input = Parameters<SubmitFunction>[0];
 type Opts = Parameters<Exclude<Awaited<ReturnType<SubmitFunction>>, void>>[0];
 
-type ReturnCallback<A extends ActionResult> = (
-	opts: Omit<Opts, 'result'> & { result: A; submitter: HTMLElement | null }
-) => void;
+type ReturnCallback<A extends ActionResult> = (opts: Omit<Opts, 'result'> & { result: A }) => void;
 
 type Options<A extends ActionResult> = Partial<{
 	delay: number;
+	disableSubmitter: boolean;
 	modalFormState: ReturnType<typeof createModalFormState>;
 	submittedCallback: ChangeReturnType<SubmitFunction, void>;
 	respondedCallback: ReturnCallback<A>;
 	completedCallback: ReturnCallback<A>;
 }>;
 
+const submitterCanBeDisabled = (submitter: HTMLElement | null) =>
+	// Reference https://developer.mozilla.org/en-US/docs/Web/API/SubmitEvent/submitter
+	// Reference https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled
+	submitter instanceof HTMLButtonElement || submitter instanceof HTMLInputElement;
+
+const disableSubmitter = (submitter: HTMLElement | null, disable = true) => {
+	if (submitterCanBeDisabled(submitter)) submitter.disabled = disable;
+};
+
 export const createSubmitFunction = <A extends ActionResult>(options: Options<A> = {}) =>
 	(async (input: Input) => {
-		const o: Options<A> = { delay: 1000, ...options };
+		const o: Options<A> = { delay: 1000, disableSubmitter: true, ...options };
+		if (o.disableSubmitter) disableSubmitter(input.submitter);
 		const timer = o.delay && new Promise((resolve) => setTimeout(resolve, o.delay));
 		await o.submittedCallback?.(input); // TODO Form submission can be canceled.
 		o.modalFormState?.set.submitting();
 		return async (opts: Opts) => {
 			if (timer) await timer;
-			const o2 = {
-				...opts,
-				result: opts.result as A,
-				submitter: input.submitter
-			} satisfies Parameters<ReturnCallback<A>>[0];
+			const o2 = { ...opts, result: opts.result as A } satisfies Parameters<ReturnCallback<A>>[0];
 			await (o.respondedCallback ? o.respondedCallback(o2) : opts.update());
+			if (o.disableSubmitter) disableSubmitter(input.submitter, false);
 			o.modalFormState?.set.submitted();
 			if (o.completedCallback) {
 				await tick(); // Wait for the form prop to be updated.
