dfa34e93718acce4d66904264ac79f6394ff5a37
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-11-08 20:31:00 +0900

    test: open modal before hydration

--- e2e/modal.test.ts
+++ e2e/modal.test.ts
@@ -1,60 +1,73 @@
 import { expect, test } from '@playwright/test';
 
-test('modal functionality', async ({ page }) => {
+test('modal component features', async ({ page }) => {
 	await page.goto('/');
-
-	//
 
 	const modal = page.locator('dialog');
 
-	const openButton = page.locator('button[data-testid="open"]');
-	const closeButton = page.locator('button[data-testid="close"]');
-	const submitButton = page.locator('button[data-testid="submit"]');
+	const button = {
+		open: page.locator('button[data-testid="open"]'),
+		close: page.locator('button[data-testid="close"]'),
+		submit: page.locator('button[data-testid="submit"]'),
+	};
 
-	const backdropCheckbox = page.locator('input[type="checkbox"][data-testid="backdrop"]');
-	const escCheckbox = page.locator('input[type="checkbox"][data-testid="esc"]');
+	const checkbox = {
+		closeWithBackdrop: page.locator('input[type="checkbox"][data-testid="backdrop"]'),
+		closeWithEscapeKey: page.locator('input[type="checkbox"][data-testid="esc"]'),
+	};
 
-	//
-
-	const consoleMessages: string[] = [];
-	page.on('console', (consoleMessage) => consoleMessages.push(consoleMessage.text()));
-
-	await openButton.click();
+	await button.open.click();
 	await expect(modal).toHaveAttribute('open');
 
-	await closeButton.click();
+	await button.close.click();
 	await modal.waitFor({ state: 'hidden' });
 	await expect(modal).not.toHaveAttribute('open');
-	expect(consoleMessages).toEqual(['closing', 'closed']);
 
-	await openButton.click();
-	await submitButton.click();
+	const consoleMessages = (await page.consoleMessages()).filter((msg) => msg.type() === 'log');
+	expect(consoleMessages.length).toEqual(2);
+	expect(consoleMessages.map((msg) => msg.text())).toEqual(['closing', 'closed']);
+
+	await button.open.click();
+	await button.submit.click();
 	await expect(modal).not.toHaveAttribute('open');
 
-	await backdropCheckbox.uncheck();
-	await openButton.click();
+	await checkbox.closeWithBackdrop.uncheck();
+	await button.open.click();
 	await page.mouse.click(0, 0);
 	await expect(modal).toHaveAttribute('open');
-	await closeButton.click();
+	await button.close.click();
 
 	// Elements outside of the modal cannot be focused. The modal must first be closed.
 	// Reference https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inert
 
-	await backdropCheckbox.check();
-	await openButton.click();
+	await checkbox.closeWithBackdrop.check();
+	await button.open.click();
 	await page.mouse.click(0, 0);
 	await expect(modal).not.toHaveAttribute('open');
 
-	await escCheckbox.uncheck();
-	await openButton.click();
-	// Reference https://github.com/sveltejs/svelte/pull/15458
-	await page.keyboard.press('Escape');
+	await checkbox.closeWithEscapeKey.uncheck();
+	await button.open.click();
 	await page.keyboard.press('Escape');
 	await expect(modal).toHaveAttribute('open');
-	await closeButton.click();
+	await button.close.click();
 
-	await escCheckbox.check();
-	await openButton.click();
+	await checkbox.closeWithEscapeKey.check();
+	await button.open.click();
 	await page.keyboard.press('Escape');
 	await expect(modal).not.toHaveAttribute('open');
+});
+
+test('modal open via client-side navigation', async ({ page }) => {
+	await page.goto('/');
+	await page.click('a[href*="is-open"]');
+	await page.waitForURL('/is-open');
+
+	const modal = page.locator('dialog');
+	await expect(modal).toHaveAttribute('open');
+});
+
+test('modal open via server-side rendering', async ({ page }) => {
+	await page.goto('/is-open');
+	const modal = page.locator('dialog');
+	await expect(modal).toHaveAttribute('open');
 });
